de70cda27415a77bdcd3b1e10ed6a47e6e839df5
Sian L. Dela Cruz <sdelacruz@reddragonfarm.com>
2023-12-21 16:38:31 +0800

    asdasdas

--- src/pages/sales-management/SpecialDiscount.jsx
+++ src/pages/sales-management/SpecialDiscount.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Checkbox, TextField, Typography } from "@mui/material";
 import React, { useContext, useState } from "react";
 import PageHeaderTabs from "../../components/PageHeaderTabs";
 import { AppContext } from "../../context/AppContext";
@@ -7,8 +7,11 @@ import useDisclosure from "../../hooks/useDisclosure";
 import CommonTable from "../../components/CommonTable";
 import CommonTableSkeleton from "../../components/CommonTableSkeleton";
 import { dummyTableData } from "../../utils/DummyData";
+import CommonDrawer from "../../components/CommonDrawer";
+import ControlledAutocomplete from "../../components/ControlledAutocomplete";
 
 function SpecialDiscount() {
+  const [drawerMode, setDrawerMode] = useState("add");
   const [tabViewing, setTabViewing] = useState(1);
   const [search, setSearch] = useState("");
   // const [listingFeeStatus, setListingFeeStatus] = useState("Under review");
@@ -24,6 +27,12 @@ function SpecialDiscount() {
     isOpen: isSpDiscountOpen,
     onOpen: onSpDiscountOpen,
     onClose: onSpDiscountClose,
+  } = useDisclosure();
+
+  const {
+    isOpen: isDrawerOpen,
+    onOpen: onDrawerOpen,
+    onClose: onDrawerClose,
   } = useDisclosure();
 
   //Constants
@@ -50,43 +59,140 @@ function SpecialDiscount() {
 
   const isFetching = false;
 
+  //Functions
+  const handleAddOpen = () => {
+    setDrawerMode("add");
+    onDrawerOpen();
+  };
+
+  const handleDrawerClose = () => {
+    // reset();
+    onDrawerClose();
+    setSelectedId("");
+  };
+
   return (
-    <Box className="commonPageLayout">
-      <PageHeaderTabs
-        wide
-        pageTitle="Special Discount"
-        tabsList={spDiscountNavigation}
-        tabViewing={tabViewing}
-        setTabViewing={setTabViewing}
-      />
-
-      <AddSearchMixin
-        addTitle="Special Discount"
-        onAddOpen={onSpDiscountOpen}
-        setSearch={setSearch}
-      />
-
-      {isFetching ? (
-        <CommonTableSkeleton moreCompact />
-      ) : (
-        <CommonTable
-          mapData={dummyTableData}
-          moreCompact
-          // excludeKeysDisplay={excludeKeysDisplay}
-          count={count}
-          rowsPerPage={rowsPerPage}
-          setRowsPerPage={setRowsPerPage}
-          page={page}
-          setPage={setPage}
-          editable
-          // onView={onViewOpen}
-          // tableHeads={tableHeads}
-          // pesoArray={pesoArray}
-          // onEdit={handleOpenEdit}
-          // onHistory={onHistoryOpen}
+    <>
+      <Box className="commonPageLayout">
+        <PageHeaderTabs
+          wide
+          pageTitle="Special Discount"
+          tabsList={spDiscountNavigation}
+          tabViewing={tabViewing}
+          setTabViewing={setTabViewing}
         />
-      )}
-    </Box>
+
+        <AddSearchMixin
+          addTitle="Special Discount"
+          onAddOpen={onDrawerOpen}
+          setSearch={setSearch}
+        />
+
+        {isFetching ? (
+          <CommonTableSkeleton moreCompact />
+        ) : (
+          <CommonTable
+            mapData={dummyTableData}
+            moreCompact
+            // excludeKeysDisplay={excludeKeysDisplay}
+            count={count}
+            rowsPerPage={rowsPerPage}
+            setRowsPerPage={setRowsPerPage}
+            page={page}
+            setPage={setPage}
+            editable
+            // onView={onViewOpen}
+            // tableHeads={tableHeads}
+            // pesoArray={pesoArray}
+            // onEdit={handleOpenEdit}
+            // onHistory={onHistoryOpen}
+          />
+        )}
+      </Box>
+
+      <CommonDrawer
+        open={isDrawerOpen}
+        onClose={handleDrawerClose}
+        drawerHeader={
+          (drawerMode === "add" ? "Add" : "Edit") + " Special Discount"
+        }
+        // onSubmit={handleSubmit(onDrawerSubmit)}
+        // disableSubmit={!isValid}
+        // isLoading={drawerMode === "add" ? isAddLoading : isUpdateLoading}
+      >
+        {/* <ControlledAutocomplete
+          name={`clientId`}
+          control={control}
+          options={clientData?.regularClient || []}
+          getOptionLabel={(option) =>
+            option.businessName + " - " + option.ownersName || ""
+          }
+          disableClearable
+          loading={isClientLoading}
+          disabled={redirect || editMode}
+          // value={clientData?.regularClient?.find(
+          //   (item) => item.businessName === selectedRowData?.businessName
+          // )}
+          // isOptionEqualToValue={(option, value) => option.id === value.id}
+          isOptionEqualToValue={(option, value) => true}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              size="small"
+              label="Business Name - Owner's Name"
+              required
+              helperText={errors?.itemId?.message}
+              error={errors?.itemId}
+              // sx={{ width: "300px" }}
+              sx={{ width: "400px" }}
+            />
+          )}
+          onChange={(_, value) => {
+            if (watch("clientId") && watch("listingItems")[0]?.itemId) {
+              onClientConfirmationOpen();
+              setConfirmationValue(value);
+              return watch("clientId");
+            } else {
+              return value;
+            }
+          }}
+        /> */}
+
+        <Box sx={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+          <Typography sx={{ fontWeight: "600", fontSize: "18px" }}>
+            Customer Name
+          </Typography>
+          <TextField
+            label="Customer Name"
+            size="small"
+            autoComplete="off"
+            // {...register("storeTypeName")}
+            // helperText={errors?.storeTypeName?.message}
+            // error={errors?.storeTypeName}
+          />
+        </Box>
+
+        <Box sx={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+          <Typography sx={{ fontWeight: "600", fontSize: "18px" }}>
+            Special Discount
+          </Typography>
+          <TextField
+            label="Special Discount"
+            size="small"
+            autoComplete="off"
+            type="number"
+            // {...register("storeTypeName")}
+            // helperText={errors?.storeTypeName?.message}
+            // error={errors?.storeTypeName}
+          />
+        </Box>
+
+        <Box sx={{ display: "flex", gap: "5px" }}>
+          <Checkbox />
+          <Typography>One time use only</Typography>
+        </Box>
+      </CommonDrawer>
+    </>
   );
 }
 
