659b6b7f9f16727b1ac7f03e8af1f8c273056212
Sian L. Dela Cruz <sdelacruz@reddragonfarm.com>
2024-01-30 11:02:58 +0800

    feat: transactions list datepickers

--- src/components/sales-transaction/TransactionsList.jsx
+++ src/components/sales-transaction/TransactionsList.jsx
@@ -4,22 +4,37 @@ import { Box, TextField, Typography } from "@mui/material";
 import { KeyboardDoubleArrowLeft } from "@mui/icons-material";
 import CommonTable from "../CommonTable";
 import { dummyTransactionsData } from "../../utils/DummyData";
-import { DatePicker } from "@mui/lab";
-import { LocalizationProvider } from "@mui/x-date-pickers";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import moment from "moment";
+import SecondaryButton from "../SecondaryButton";
 
 function TransactionsList({ setTransactionsMode }) {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [dateFrom, setDateFrom] = useState(moment());
+  const [dateTo, setDateTo] = useState(moment());
+  const [dateFromTemp, setDateFromTemp] = useState(moment());
+  const [dateToTemp, setDateToTemp] = useState(moment());
 
   //Constants
   const pesoArray = ["amount"];
+
+  //Functions
+  const handleSubmitDate = () => {
+    setDateFrom(dateFromTemp);
+    setDateTo(dateToTemp);
+  };
+
+  console.log("Date From: ", dateFrom);
+  console.log("Date To: ", dateTo);
 
   return (
     <Box className="transactionsList">
       <Box className="transactionsList__header">
         <Box className="transactionsList__header__left">
           <DangerButton
+            size="medium"
             onClick={() => {
               setTransactionsMode(false);
               sessionStorage.removeItem("transactionsMode");
@@ -37,22 +52,36 @@ function TransactionsList({ setTransactionsMode }) {
           <LocalizationProvider dateAdapter={AdapterMoment}>
             <DatePicker
               label="Date From"
+              value={dateFromTemp}
+              onChange={setDateFromTemp}
               slotProps={{
-                textField: { size: "small", required: true },
+                textField: { size: "small" },
               }}
+              sx={{ width: "180px", whiteSpace: "pre-wrap", p: 0, m: 0 }}
               renderInput={(params) => <TextField {...params} />}
             />
           </LocalizationProvider>
 
           <LocalizationProvider dateAdapter={AdapterMoment}>
             <DatePicker
-              label="Date From"
+              label="Date To"
+              value={dateToTemp}
+              onChange={setDateToTemp}
               slotProps={{
-                textField: { size: "small", required: true },
+                textField: { size: "small" },
               }}
+              sx={{ width: "180px" }}
               renderInput={(params) => <TextField {...params} />}
             />
           </LocalizationProvider>
+
+          <SecondaryButton
+            size="medium"
+            onClick={handleSubmitDate}
+            sx={{ ml: "10px" }}
+          >
+            Submit
+          </SecondaryButton>
         </Box>
       </Box>
 
