119ddd701462dadf38b229fe2aa21b6642c847e1
Sian L. Dela Cruz <sdelacruz@reddragonfarm.com>
2024-03-25 16:56:52 +0800

    asdasda

--- src/components/modals/sales-management/PaymentHistoriesModal.jsx
+++ src/components/modals/sales-management/PaymentHistoriesModal.jsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Collapse,
   IconButton,
   Step,
   StepContent,
@@ -9,11 +10,14 @@ import {
 } from "@mui/material";
 import CommonModal from "../../CommonModal";
 import moment from "moment";
-import { KeyboardArrowDown } from "@mui/icons-material";
+import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
+import { useState } from "react";
 
 function PaymentHistoriesModal({ ...props }) {
+  const [expandedPaymentType, setExpandedPaymentType] = useState(false);
+
   return (
-    <CommonModal {...props} closeTopRight>
+    <CommonModal {...props} closeTopRight height="70vh">
       <Box className="paymentHistoriesModal">
         <Typography className="paymentHistoriesModal__title">
           Payment Histories
@@ -31,10 +35,98 @@ function PaymentHistoriesModal({ ...props }) {
               </StepLabel>
 
               <StepContent>
-                Total: ₱200,000.00{" "}
-                <IconButton>
-                  <KeyboardArrowDown />
-                </IconButton>
+                <Box className="paymentHistoriesModal__body__stepContent">
+                  <Box className="paymentHistoriesModal__body__total">
+                    <Typography className="paymentHistoriesModal__body__total__label">
+                      Total:
+                    </Typography>
+
+                    <Typography className="paymentHistoriesModal__body__total__value">
+                      ₱200,000.00
+                    </Typography>
+                  </Box>
+
+                  <Box
+                    className="paymentHistoriesModal__body__paymentTypes"
+                    onClick={() => setExpandedPaymentType((prev) => !prev)}
+                  >
+                    <Typography className="paymentHistoriesModal__body__paymentTypes__label">
+                      Payment Type(s):
+                    </Typography>
+
+                    <Typography className="paymentHistoriesModal__body__paymentTypes__value">
+                      Cash, Online
+                    </Typography>
+
+                    <IconButton>
+                      {expandedPaymentType ? (
+                        <KeyboardArrowUp />
+                      ) : (
+                        <KeyboardArrowDown />
+                      )}
+                    </IconButton>
+                  </Box>
+
+                  <Collapse in={expandedPaymentType}>
+                    <Box className="paymentHistoriesModal__body__paymentsDetailed">
+                      <Box className="paymentHistoriesModal__body__paymentsDetailed__item">
+                        <Typography className="paymentHistoriesModal__body__paymentsDetailed__item__title">
+                          Cash
+                        </Typography>
+
+                        <Box className="paymentHistoriesModal__body__paymentsDetailed__item__content">
+                          <Box className="paymentHistoriesModal__body__paymentsDetailed__item__content__row">
+                            <Typography className="paymentHistoriesModal__body__paymentsDetailed__item__content__row__label">
+                              Amount:
+                            </Typography>
+
+                            <Typography className="paymentHistoriesModal__body__paymentsDetailed__item__content__row__value">
+                              ₱20,000.00
+                            </Typography>
+                          </Box>
+                        </Box>
+                      </Box>
+
+                      <Box className="paymentHistoriesModal__body__paymentsDetailed__item">
+                        <Typography className="paymentHistoriesModal__body__paymentsDetailed__item__title">
+                          Online
+                        </Typography>
+
+                        <Box className="paymentHistoriesModal__body__paymentsDetailed__item__content">
+                          <Box className="paymentHistoriesModal__body__paymentsDetailed__item__content__row">
+                            <Typography className="paymentHistoriesModal__body__paymentsDetailed__item__content__row__label">
+                              Amount:
+                            </Typography>
+
+                            <Typography className="paymentHistoriesModal__body__paymentsDetailed__item__content__row__value">
+                              ₱20,000.00
+                            </Typography>
+                          </Box>
+
+                          <Box className="paymentHistoriesModal__body__paymentsDetailed__item__content__row">
+                            <Typography className="paymentHistoriesModal__body__paymentsDetailed__item__content__row__label">
+                              Account Number:
+                            </Typography>
+
+                            <Typography className="paymentHistoriesModal__body__paymentsDetailed__item__content__row__value">
+                              09984461402
+                            </Typography>
+                          </Box>
+
+                          <Box className="paymentHistoriesModal__body__paymentsDetailed__item__content__row">
+                            <Typography className="paymentHistoriesModal__body__paymentsDetailed__item__content__row__label">
+                              Reference Number:
+                            </Typography>
+
+                            <Typography className="paymentHistoriesModal__body__paymentsDetailed__item__content__row__value">
+                              10000239292
+                            </Typography>
+                          </Box>
+                        </Box>
+                      </Box>
+                    </Box>
+                  </Collapse>
+                </Box>
               </StepContent>
             </Step>
 
