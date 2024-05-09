04bdc1a2d61fa96560f7823cf2c2e1411b944f33
sian022 <delacruzsian22@gmail.com>
2024-05-09 10:42:54 +0800

    asdads

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -199,7 +199,7 @@ function LoginPage() {
                 {isLoading ? (
                   <CircularProgress size="20px" color="white" />
                 ) : (
-                  "Sign In"
+                  "Sign On"
                 )}
               </SecondaryButton>
             </Box>
