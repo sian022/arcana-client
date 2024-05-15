4669690cce85290f23572000eef7c35f2cedc57e
sian022 <delacruzsian22@gmail.com>
2024-05-15 11:35:42 +0800

    asdasda

---
+++ cypress/e2e/0-approval/approveRegistration.cy.js
@@ -0,0 +1,71 @@
+/// <reference types="cypress" />
+
+describe("Approve Registration", () => {
+  beforeEach(() => {
+    cy.login("djjimenez", "1234");
+  });
+
+  it("should navigate to registration approval page, and approve a registration", () => {
+    // Navigate to registration approval page
+    cy.openApprovalPage();
+    cy.url().should("include", "/approval/registration-approval");
+
+    // Approve a registration
+    cy.intercept("PUT", "/RegularClients/ApproveClientRegistration/*").as(
+      "approveRegistration"
+    );
+
+    cy.approveRegistration();
+
+    // Wait for the response
+    cy.wait("@approveRegistration").then((interception) => {
+      const responseData = interception.response.body.value;
+      if (interception.response.statusCode === 200) {
+        cy.contains("Registration approved successfully").should(
+          "be.visible",
+          "Registration approved successfully"
+        );
+      } else {
+        cy.contains("Registration failed to approve").should(
+          "be.visible",
+          "failed"
+        );
+      }
+    });
+  });
+});
+
+// Define custom commands
+Cypress.Commands.add("login", (username, password) => {
+  cy.visit("http://localhost:5111/login");
+  cy.get('input[name="username"]').type(username);
+  cy.get('input[name="password"]').type(password);
+  cy.get('button[type="submit"]').click();
+
+  // Ensure successful login
+  cy.url().should("equal", "http://localhost:5111/");
+  cy.get(".dashboard__top__greetingCard__greeting").should(
+    "contain",
+    "Fresh morning!"
+  );
+});
+
+Cypress.Commands.add("openApprovalPage", () => {
+  cy.get('a[href="/approval"]').then(($link) => {
+    if ($link.is(":visible")) {
+      cy.get('a[href="/approval"]').click();
+      cy.get('a[href="/approval/registration-approval"]').click();
+    } else {
+      cy.get('button[aria-label="toggle sidebar"]').click();
+      cy.get('a[href="/approval"]').click();
+      cy.get('a[href="/approval/registration-approval"]').click();
+      cy.get(".sidebar__toggleRemove").click();
+    }
+  });
+});
+
+Cypress.Commands.add("approveRegistration", () => {
+  cy.get(".approval__table__row").first().click();
+  cy.get('button[type="button"]').contains("Approve").click();
+  cy.get('button[type="submit"]').click();
+});
